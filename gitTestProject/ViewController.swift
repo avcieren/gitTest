56ca15a4eb700aeba3c68c90a3c8293b445522a5
Eren Avcı <erenavci@eren-macbook-pro.local>
2020-08-13 20:57:08 +0300

    Initial Commit

---
+++ gitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTestProject
+//
+//  Created by Eren Avcı on 13.08.2020.
+//  Copyright © 2020 Eren Avcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
